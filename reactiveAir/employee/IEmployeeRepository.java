9d46ba781aaf1a35d321373bb8771a30a7b65816
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-02 20:00:38 -0600

    Update IEmployeeRepository.java

--- reactiveAir/employee/IEmployeeRepository.java
+++ reactiveAir/employee/IEmployeeRepository.java
@@ -1,30 +1,21 @@
 package com.dz_fs_dev.airlines.reactiveAir.employee;
 
-import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 /**
  * Persistence layer for {@link Employee} entities.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.1
+ * @version 0.0.2
  */
-@Repository
 public interface IEmployeeRepository  extends ReactiveCrudRepository<Employee, Long>{
-	@Query("SELECT * FROM employee WHERE last_name = :lastname")
+    Flux<Employee> findByFirstName(String firstName);
+	
     Flux<Employee> findByLastName(String lastName);
-
-	@Query("SELECT * FROM employee WHERE id = :id")
-	Mono<Employee> findEmployeeById(String id);
-
-	@Query("SELECT * FROM employee")
-	Flux<Employee> findAllEmployees();
-
-	@Query("SELECT * FROM employee")
-	Mono<Employee> updateEmployee(Employee employee);
+    
+    Flux<Employee> findByFirstNameOrderByFirstName(String firstName);
+	
+    Flux<Employee> findByLastNameOrderByFirstName(String lastName);
 }
