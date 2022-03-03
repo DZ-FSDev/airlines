d3acefe5fe17c4feaf1bb506ac3e68d5bb4ea5a4
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-02 22:19:07 -0600

    Add job field; annotate builder

--- reactiveAir/employee/Employee.java
+++ reactiveAir/employee/Employee.java
@@ -2,7 +2,9 @@ package com.dz_fs_dev.airlines.reactiveAir.employee;
 
 import org.springframework.data.annotation.Id;
 
+import com.dz_fs_dev.airlines.reactiveAir.job.Job;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,13 +13,16 @@ import lombok.NoArgsConstructor;
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.2
+ * @version 0.0.3
  */
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Employee {
 	private @Id Long id;
 	private String firstName;
 	private String lastName;
+	
+	private Job job;
 }
