91c133a7c1cc715fc2997f66f1822799514af555
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-02 22:04:39 -0600

    Update JobHistory.java

--- reactiveAir/jobHistory/JobHistory.java
+++ reactiveAir/jobHistory/JobHistory.java
@@ -1,19 +1,29 @@
 package com.dz_fs_dev.airlines.reactiveAir.jobHistory;
 
+import java.math.BigDecimal;
+
 import org.springframework.data.annotation.Id;
 
-import com.dz_fs_dev.airlines.reactiveAir.job.Job;
+import com.dz_fs_dev.airlines.reactiveAir.employee.Employee;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+/**
+ * Represents a past job that an employee held.
+ * 
+ * @author DZ-FSDev
+ * @since 17.0.2
+ * @version 0.0.2
+ */
 @Data
-@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
-public class JobHistory extends Job{
+public class JobHistory{
 	private @Id Long id;
+	private Employee employee;
+	private String title;
+	private BigDecimal salary;
 	private Long endTS;
 }
