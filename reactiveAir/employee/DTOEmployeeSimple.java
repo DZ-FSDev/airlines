a6cbe48e1c8e299551a58d751b1d152335977d14
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-06 23:17:59 -0600

    Create DTOEmployeeSimple.java

---
+++ reactiveAir/employee/DTOEmployeeSimple.java
@@ -0,0 +1,19 @@
+package com.dz_fs_dev.airlines.reactiveAir.employee;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Simple DTO for employees used as a class-based projection to reveal
+ * minimum identification for an Employee.
+ * 
+ * @author DZ-FSDev
+ * @since 17.0.2
+ * @version 0.0.1
+ */
+@Data
+@AllArgsConstructor
+public class DTOEmployeeSimple {
+	private String firstName;
+	private String lastName;
+}
