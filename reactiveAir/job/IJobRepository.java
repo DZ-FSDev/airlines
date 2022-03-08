f6e72ff1738e619ecf7da58abc18f33211a5b816
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-08 16:20:45 -0600

    Update IJobRepository.java

--- reactiveAir/job/IJobRepository.java
+++ reactiveAir/job/IJobRepository.java
@@ -2,13 +2,16 @@ package com.dz_fs_dev.airlines.reactiveAir.job;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
+import reactor.core.publisher.Flux;
+
 /**
  * Persistence layer for {@link Job} entities.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.1
+ * @version 0.0.2
  */
 public interface IJobRepository  extends ReactiveCrudRepository<Job, Long>{
-
+	<T extends IJob> Flux<T> findAllBy(Class<T> type);
+	<T extends IJob> Flux<T> findAllByTitle(Class<T> type, String title);
 }
