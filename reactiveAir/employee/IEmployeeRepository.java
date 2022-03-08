a31e634e8e81ced89aaef736da31d7afc9de24a2
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-08 15:43:49 -0600

    Decorator

--- reactiveAir/employee/IEmployeeRepository.java
+++ reactiveAir/employee/IEmployeeRepository.java
@@ -8,14 +8,14 @@ import reactor.core.publisher.Flux;
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.5
+ * @version 0.0.6
  */
 public interface IEmployeeRepository  extends ReactiveCrudRepository<Employee, Long>{
     
-	<T> Flux<T> findAllBy(Class<T> type);
-	<T> Flux<T> findByFirstName(String firstName, Class<T> type);
-	<T> Flux<T> findByLastName(String lastName, Class<T> type);
-	<T> Flux<T> findByFirstNameOrderByFirstName(String lastName, Class<T> type);
-	<T> Flux<T> findByLastNameOrderByFirstName(String lastName, Class<T> type);
+	<T extends IEmployee> Flux<T> findAllBy(Class<T> type);
+	<T extends IEmployee> Flux<T> findByFirstName(String firstName, Class<T> type);
+	<T extends IEmployee> Flux<T> findByLastName(String lastName, Class<T> type);
+	<T extends IEmployee> Flux<T> findByFirstNameOrderByFirstName(String lastName, Class<T> type);
+	<T extends IEmployee> Flux<T> findByLastNameOrderByFirstName(String lastName, Class<T> type);
 	
 }
