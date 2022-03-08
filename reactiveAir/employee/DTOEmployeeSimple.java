d4e9ce621a0f369cd1df74d819c7c2ab732ebeb5
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-08 15:39:23 -0600

    Add Copyright

--- reactiveAir/employee/DTOEmployeeSimple.java
+++ reactiveAir/employee/DTOEmployeeSimple.java
@@ -1,3 +1,21 @@
+/*  Original Licensing Copyright
+ * 
+ *  DTO for employees to reveal minimum identification.
+ *  Copyright (C) 2022  DZ-FSDev
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.dz_fs_dev.airlines.reactiveAir.employee;
 
 import lombok.AllArgsConstructor;
@@ -5,8 +23,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * Simple DTO for employees used as a class-based projection to reveal
- * minimum identification for an Employee.
+ * Simple DTO used as a class-based projection to reveal minimum identification
+ * for an Employee.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
