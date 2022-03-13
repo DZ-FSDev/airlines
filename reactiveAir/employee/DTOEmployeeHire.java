3511bda84eb79a05dd0d955a70a4051d1e602c22
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-12 22:18:50 -0600

    Create DTOEmployeeHire.java

---
+++ reactiveAir/employee/DTOEmployeeHire.java
@@ -0,0 +1,40 @@
+/*  Original Licensing Copyright
+ * 
+ *  DTO for employees to reveal minimum identification for hiring.
+ *  Copyright (C) 2022  DZ-FSDev
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.dz_fs_dev.airlines.reactiveAir.employee;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Simple DTO used as a class-based projection to reveal minimum identification
+ * for an Employee to be hired.
+ * 
+ * @author DZ-FSDev
+ * @since 17.0.2
+ * @version 0.0.1
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DTOEmployeeHire implements IEmployee{
+	private String firstName;
+	private String lastName;
+	private Long startTS;
+}
