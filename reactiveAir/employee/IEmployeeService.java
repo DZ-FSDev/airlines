d3df61e07925ff29ec0184e43eb5edd580e11c6c
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-13 15:24:30 -0500

    Create IEmployeeService.java

---
+++ reactiveAir/employee/IEmployeeService.java
@@ -0,0 +1,28 @@
+package com.dz_fs_dev.airlines.reactiveAir.employee;
+
+import org.springframework.stereotype.Service;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * The user of this interface should implement the various actions associated
+ * with an employee service.
+ * 
+ * @author DZ-FSDev
+ * @since 17.0.2
+ * @version 0.0.1
+ */
+@Service
+public interface IEmployeeService {
+	<T extends IEmployee> Flux<T> findAllBy(Class<T> type);
+	<T extends IEmployee> Flux<T> findByFirstName(String firstName, Class<T> type);
+	<T extends IEmployee> Flux<T> findByLastName(String lastName, Class<T> type);
+	<T extends IEmployee> Flux<T> findByFirstNameOrderByFirstName(String lastName, Class<T> type);
+	<T extends IEmployee> Flux<T> findByLastNameOrderByFirstName(String lastName, Class<T> type);
+	Mono<Employee> save(Employee emp);
+	Mono<Employee> findById(long parseLong);
+	Mono<Long> count();
+	
+	
+}
