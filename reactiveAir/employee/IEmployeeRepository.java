1506f2eb2dea909fc0ba82ed73a684ecc889f23a
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-14 22:22:11 -0500

    + findAllSupervisedEmployees(Class<T>) : Flux<T>

--- reactiveAir/employee/IEmployeeRepository.java
+++ reactiveAir/employee/IEmployeeRepository.java
@@ -1,18 +1,20 @@
 package com.dz_fs_dev.airlines.reactiveAir.employee;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Persistence layer for {@link Employee} entities.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.10
+ * @version 0.0.11
  */
 public interface IEmployeeRepository  extends ReactiveCrudRepository<Employee, Long>{
-	
-    	<T extends IEmployee> Mono<T> findById(Class<T> type, Long id);
+
+	<T extends IEmployee> Mono<T> findById(Class<T> type, Long id);
 	<T extends IEmployee> Flux<T> findAllBy(Class<T> type);
 	<T extends IEmployee> Flux<T> findByFirstName(String firstName, Class<T> type);
 	<T extends IEmployee> Flux<T> findByLastName(String lastName, Class<T> type);
@@ -29,4 +31,7 @@ public interface IEmployeeRepository  extends ReactiveCrudRepository<Employee, L
 	@Query("SELECT * FROM employee e "
 			+ "JOIN employee f ON e.id = f.supervisor_id")
 	<T extends IEmployee> Flux<T> findAllSupervisors(Class<T> type);
+	
+	@Query("SELECT * FROM employee e WHERE e.supervisor_id IS NOT NULL")
+	<T extends IEmployee> Flux<T> findAllSupervisedEmployees(Class<T> type);
 }
