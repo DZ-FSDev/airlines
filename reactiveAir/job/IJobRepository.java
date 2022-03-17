1dd91d4c94b60c709cf5c5668e10513a41d19896
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-17 08:28:30 -0500

    + findAllBySalaryLessThan(Class<T>, BigDecimal) : Flux<T>

--- reactiveAir/job/IJobRepository.java
+++ reactiveAir/job/IJobRepository.java
@@ -1,8 +1,11 @@
 package com.dz_fs_dev.airlines.reactiveAir.job;
 
+import java.math.BigDecimal;
+
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Persistence layer for {@link Job} entities.
@@ -15,11 +18,10 @@ public interface IJobRepository  extends ReactiveCrudRepository<Job, Long>{
 	<T extends IJob> Mono<T> findById(Class<T> type, Long id);
 	<T extends IJob> Flux<T> findAllBy(Class<T> type);
 	<T extends IJob> Flux<T> findAllByTitle(Class<T> type, String title);
-	
-	<T extends IJob> Flux<T> findAllByTitle(Class<T> type, String title);
 	<T extends IJob> Flux<T> findAllByTitleOrderBySalary(Class<T> type, String title);
 	
 	<T extends IJob> Flux<T> findAllBySalaryGreaterThan(Class<T> type, BigDecimal salary);
+	<T extends IJob> Flux<T> findAllBySalaryLessThan(Class<T> type, BigDecimal salary);
 	
 	<T extends IJob> Flux<T> findAllBySalaryBetween(Class<T> type, BigDecimal fromSalary, BigDecimal toSalary);
 }
