48b682d751edafd425bce89b670a396752f7243a
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-20 11:34:12 -0500

    Add audit fields

--- reactiveAir/employee/Employee.java
+++ reactiveAir/employee/Employee.java
@@ -1,25 +1,31 @@
 package com.dz_fs_dev.airlines.reactiveAir.employee;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 /**
  * Represents an airline employee.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.6
+ * @version 0.0.7
  */
 @Table
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Employee implements IEmployee{
 	private @Id Long id;
 	
@@ -32,4 +38,13 @@ public class Employee implements IEmployee{
 	
 	private Long supervisor_id;
 	private Long manager_id;
+
+	@CreatedDate
+	private Long createdAt;
+	@LastModifiedDate
+	private Long lastModified;
+	@CreatedBy
+	private String creator;
+	@LastModifiedBy
+	private String modifier;
 }
