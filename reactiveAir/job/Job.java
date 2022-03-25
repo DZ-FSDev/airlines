1c2ff6a804eb8f2a9fdfe0205f084e2b8e7acd4a
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-25 08:43:13 -0500

    Upd to 0.0.4

--- reactiveAir/job/Job.java
+++ reactiveAir/job/Job.java
@@ -3,6 +3,7 @@ package com.dz_fs_dev.airlines.reactiveAir.job;
 import java.math.BigDecimal;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,13 +11,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * Represents a job at an airline.
+ * Represents a job at an airport.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.3
+ * @version 0.0.4
  */
 @Data
+@Table
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -25,4 +27,6 @@ public class Job implements IJob{
 	private String title;
 	private BigDecimal salary;
 	private Long endTS;
+	
+	private Long employee_id;
 }
