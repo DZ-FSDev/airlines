aeb4c0c1ff6e1fcc3313db85ea056427bf1d4e23
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-28 08:38:39 -0500

    ext. R2dbcRepository

--- reactiveAir/gate/IGateRepository.java
+++ reactiveAir/gate/IGateRepository.java
@@ -1,14 +1,14 @@
 package com.dz_fs_dev.airlines.reactiveAir.gate;
 
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
 /**
  * Reactive Persistence layer for {@link Gate} entities.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.1
+ * @version 0.0.2
  */
-public interface IGateRepository  extends ReactiveCrudRepository<Gate, Long>{
+public interface IGateRepository  extends R2dbcRepository<Gate, Long>{
 
 }
